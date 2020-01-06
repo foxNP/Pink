3e5caae54ec0809bfdb85ddc74e18420e37363e9
Marina <marina123123@marina.com>
2020-01-06 19:40:23 +0300

    template

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    pagebuilder = require('gulp-pagebuilder');
+
 
 var path = {
     src: {
         style: '_assets/scss/styles.scss',
+        html: 'html/**/*.html'
     },
     build: {
         css: 'build/css/',
+        html: 'build/'
     },
     watch: {
         style: '_assets/scss/**/*.scss',
+        html: 'html/**/*.html'
     }
 };
 
@@ -17,13 +22,21 @@ function styles() {
     return gulp.src(path.src.style)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(path.build.css))
-};
+}
+
+function html() {
+    return gulp.src(path.src.html)
+        .pipe(pagebuilder('html'))
+        .pipe(gulp.dest(path.build.html));
+}
 
 gulp.task('watch', function () {
     gulp.watch(path.watch.style, styles);
+    gulp.watch(path.watch.html, html);
 });
 gulp.task('scss', gulp.series(styles));
+gulp.task('html', gulp.series(html));
 gulp.task('default', gulp.series(
-    gulp.parallel(styles),
+    gulp.parallel(styles, html),
     'watch'
 ));
